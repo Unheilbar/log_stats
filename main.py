30c24b92fb2b024b997d24dcbd134b57cafbdbda
unheilbar <vladimiramnell@gmail.com>
2020-11-11 12:12:05 +1000

    table structure changed

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+#Script for parsing logs files and writing statistics into clickhouse database
+#Example of the log' line is
+
+#2020-11-10 16:06:00  10.19.201.110   109.126.58.173  list 15    https://venetia.iad.appboy.com (151.101.12.233:443)
+
+#All constant variables are written in config.ini file (should be in the same directory with script)
+
 from configparser import ConfigParser
 from clickhouse_driver import Client
+from datetime import datetime
 
 def config(section, filename = 'config.ini'): #read constant variables
     # create a parser
@@ -7,7 +15,7 @@ def config(section, filename = 'config.ini'): #read constant variables
     # read config file
     parser.read(filename)
 
-    # get section, default to postgresql
+    # get section
     result = {}
     if parser.has_section(section):
         params = parser.items(section)
@@ -18,22 +26,29 @@ def config(section, filename = 'config.ini'): #read constant variables
 
     return result
 
-def parse_log_line(line):
+def parse_log_line(line): #function for parsing a single line from log
     splitted = line.split()
     if len(splitted) == 8:
-        date = splitted[0]
-        time = splitted[1]
+        date = datetime.strptime(' '.join([splitted[0],splitted[1]]), "%Y-%m-%d %H:%M:%S")
         ip_abon = splitted[2]
         ip_nat = splitted[3]
-        ip_nat_list = splitted[5]
-        url = splitted[6]
-        ip_server = splitted[7][1:-1]
-        return [date, time, ip_abon, ip_nat, ip_nat_list, url, ip_server]
+        id_nat_list = int(splitted[5])
+        url = splitted[6].split('://')[1]
+        protocol = splitted[6].split('://')[0]
+        if(protocol == 'http'):
+            protocol_type = 1
+        elif (protocol == 'https'):
+            protocol_type = 2
+        else:
+            protocol_type = 3
+ 
+        ip_server = splitted[7][1:-1].split(':')[0]
+
+        return {'event_date':date, 'ip_abon':ip_abon, 'ip_nat':ip_nat, 'id_nat_list':id_nat_list, 'url':url, 'ip_server':ip_server, 'protocol_type':protocol_type }
         
     return
 
-
-def parse_log_from_file(file_name):
+def parse_logs_from_file(file_name): #parse logs from a single file = filename
     parsed_logs = []
     try:
         with open(file_name) as file_handler:
@@ -47,21 +62,43 @@ def parse_log_from_file(file_name):
     except IOError:
         print("an IOError has occured")
 
+
+def getHostname(string):
+    week_number = datetime.now().isocalendar()[1]
+
+    hostnames = string.split()
+    current_hostname = hostnames[week_number % 2]     #choose current hostname for insert data in table
+                                                      #current hostname depends on parity of current week
+    
+    return current_hostname
+
 def insert_into_db():
 
-    client = Client('localhost', user = 'default', database = 'testlogs', password = 'qwerty')
-    
-    result = client.execute('SELECT now(), version()')
+    #connecting to clickhouse database
+    try:
+        login_data = config('Clickhouse_db_login')
 
-    for t in result:
-        print(" ROW: {0}: {1}".format(type(t), t))
-    for v in t:
-            print("  COLUMN: {0}: {1}".format(type(v), v))
+        hostname = getHostname(login_data['hostnames'])
+
+        user = login_data['user']
+        database = login_data['database']
+        password = login_data['password']
+
+        client = Client(host = hostname, user = user, database = database, password = password)
+        
+        print('Connected to clickhouse db')
+
+        #parsed = parse_logs_from_file('head 2020-11-10_16:06.log')
+
+        #client.execute('INSERT INTO test_logs (event_date, ip_server, url, ip_nat, ip_abon, id_nat_list, protocol_type) VALUES', parsed)
+
+        print('Command has executed successfully')
+
+    except (Exception, Client.DatabaseError) as error:
+        print(error)
 
 def main():
-    parsed = parse_log_from_file('head 2020-11-10_16:06.log')
-    for i in range(5):
-        print(parsed[i])
     insert_into_db()
+    
 
 main()
---
+++ sqlcomands.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS test_logs (
+    date date DEFAULT toDate (event_date),
+    event_date DateTime ('Asia/Vladivostok'),
+    ip_abon IPv4,
+    ip_nat IPv4,
+    id_nat_list Int32,
+    url String,
+    ip_server IPv4,
+    protocol_type UInt8 
+    )
+    ENGINE = MergeTree ()
+PARTITION BY toMonday (date)
+ORDER BY
+    (event_date, ip_abon)
+
+
+CREATE TABLE test.nat_logs (date date DEFAULT toDate (event_date), event_date DateTime, ip_abon String, ip_nat String, id_nat_list Int32, url String, url_reg String DEFAULT replaceRegexpOne (url, '[?](.)+', ''), ip_server String, uid UInt32, tariff UInt16, s days_in_block UInt16, block_type UInt8) ENGINE = ReplicatedMergeTree ('/clickhouse/tables/{shard}/test/nat_logs_replicated_bc', '{replica}')
+PARTITION BY toMonday (date)
+ORDER BY
+    (uid, ip_abon, event_date, intHash32 (id_nat_list)) SAMPLE BY
+        intHash32 (id_nat_list) SETTINGS index_granularity = 8192 
+
+@@@ @@@ @
+INSERT INTO test_logs (event_date, ip_abon, ip_nat, id_nat_list, url, ip_server)
+VALUES ('2020-11-10 16:06:00', '10.15.193.106', '109.126.4.154', '15', 'https://ad.mail.ru', '94.100.180.197:443')
