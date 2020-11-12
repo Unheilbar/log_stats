c2ba306ea23d33f71a1a48081aa105fc0f64e602
unheilbar <vladimiramnell@gmail.com>
2020-11-12 14:12:31 +1000

    using clickhouse_driver for inserts instead of subprocess

--- main.py
+++ main.py
@@ -70,7 +70,6 @@ def rename_current_log(path, name): #changes name of the current log file
 
 def getHostname(string):
     week_number = datetime.now().isocalendar()[1]
-
     hostnames = string.split()
     current_hostname = hostnames[week_number % 2]     #choose current hostname for inserting data in the table
                                                       #current hostname depends on parity of current week
@@ -94,7 +93,7 @@ def insert_into_db(parsed_data):
 
         table_name = config('Clickhouse_table_data')['table_name'] #get table name from config file
 
-        client.execute('INSERT INTO {0} (event_date, ip_server, url, ip_nat, ip_abon, protocol_type) VALUES'.format(table_name), parsed_data) #execute comand for inserting data
+        client.execute('INSERT INTO {0} (event_date, ip_server, url, ip_nat, ip_abon, protocol_type) VALUES'.format(table_name), parsed_data) #execute command for inserting data
 
         print('Command has executed successfully')
 
@@ -111,16 +110,11 @@ def update_table_data(path, max_logs_per_execute):
     for file_name in file_names:
         if(file_name.endswith('.log')):
             if(logs_count < max_logs_per_execute):
-
                 logs_count+=1
-
-                newname = rename_current_log(path, file_name)
-                
+                newname = rename_current_log(path, file_name)                
                 data = parse_logs_from_file(newname)
-
                 if(data):
                     insert_into_db(data)
-
                     delete_current_log(newname)
 
 def main():
@@ -129,6 +123,5 @@ def main():
     max_logs_per_execute = int(config('limits')['max_logs_per_execute']) #max logs for reading per one execute
 
     update_table_data(path, max_logs_per_execute) #update table data
-
 
 main()
--- sqlcomands.sql
+++ sqlcomands.sql
@@ -5,26 +5,16 @@ CREATE TABLE IF NOT EXISTS test_logs (
     ip_nat IPv4,
     url String,
     ip_server IPv4,
-    protocol_type UInt8 
-    )
-    ENGINE = MergeTree ()
+    protocol_type UInt8) ENGINE = MergeTree ()
 PARTITION BY toMonday (date)
 ORDER BY
-    (event_date, ip_abon)
-
-
-
-CREATE TABLE test.nat_logs (date date DEFAULT toDate (event_date), event_date DateTime, ip_abon String, ip_nat String, id_nat_list Int32, url String, url_reg String DEFAULT replaceRegexpOne (url, '[?](.)+', ''), ip_server String, uid UInt32, tariff UInt16, s days_in_block UInt16, block_type UInt8) ENGINE = ReplicatedMergeTree ('/clickhouse/tables/{shard}/test/nat_logs_replicated_bc', '{replica}')
+    (event_date, ip_abon) CREATE TABLE test.nat_logs (date date DEFAULT toDate (event_date), event_date DateTime, ip_abon String, ip_nat String, id_nat_list Int32, url String, url_reg String DEFAULT replaceRegexpOne (url, '[?](.)+', ''), ip_server String, uid UInt32, tariff UInt16, s days_in_block UInt16, block_type UInt8) ENGINE = ReplicatedMergeTree ('/clickhouse/tables/{shard}/test/nat_logs_replicated_bc', '{replica}')
 PARTITION BY toMonday (date)
 ORDER BY
     (uid, ip_abon, event_date, intHash32 (id_nat_list)) SAMPLE BY
-        intHash32 (id_nat_list) SETTINGS index_granularity = 8192 
-
-@@@ @@@ @
-INSERT INTO test_logs (event_date, ip_server, url, ip_nat, ip_abon, protocol_type) 
-VALUES ('2020-11-10 16:06:00',  IPv4StringToNum(10.15.208.170), IPv4StringToNum(109.126.4.58), 'history.google.com', IPv4StringToNum(64.233.165.102), '2')
-
-
-!!!!!!!!!111!!!!11111111111111111111111
-INSERT INTO test_logs (event_date, ip_abon, ip_nat, url, ip_server, protocol_type) 
-VALUES ('2020-11-10 16:06:00', '10.15.208.170', '109.126.4.58', 'history.google.com', '64.233.165.102', '2')
+        intHash32 (id_nat_list) SETTINGS index_granularity = 8192 @@@ @@@ @ INSERT INTO test_logs (event_date, ip_abon, ip_nat, id_nat_list, url, ip_server)
+            VALUES ('2020-11-10 16:06:00', '10.15.193.106', '109.126.4.154', '15', 'https://ad.mail.ru', '94.100.180.197:443') 
+            
+            
+INSERT INTO test_logs (event_date, ip_server, url, ip_nat, ip_abon, protocol_type)
+    VALUES (2020 - 11 - 10 16:06:00, 10.15.208.170, 109.126.4.58, history.google.com, 64.233.165.102, 2), (2020 - 11 - 10 16:06:00, 10.24.197.91, 109.126.59.110, spclient.wg.spotify.com, 35.186.224.25, 2), (2020 - 11 - 10 16:06:00, 10.19.196.17, 109.126.58.231, account - public - service - prod.ol.epicgames.com, 3.229.167.149, 2), (2020 - 11 - 10 16:06:00, 10.10.227.7, 109.126.36.161, c3.rbxcdn.com, 111.119.27.92, 2), (2020 - 11 - 10 16:06:00, 10.15.193.106, 109.126.4.154, ad.mail.ru, 94.100.180.197, 2)
